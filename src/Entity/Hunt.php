5bb24764c5372317fd5b96da4888ef65c5c756c1
Tolkya <jc.caillault@outlook.fr>
2025-12-30 16:39:48 +0100

    entity Question

--- src/Entity/Hunt.php
+++ src/Entity/Hunt.php
@@ -44,9 +44,16 @@ class Hunt
     #[ORM\OneToMany(targetEntity: QrCode::class, mappedBy: 'hunt', orphanRemoval: true)]
     private Collection $qrCodes;
 
+    /**
+     * @var Collection<int, Question>
+     */
+    #[ORM\OneToMany(targetEntity: Question::class, mappedBy: 'hunt', orphanRemoval: true)]
+    private Collection $questions;
+
     public function __construct()
     {
         $this->qrCodes = new ArrayCollection();
+        $this->questions = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -162,6 +169,36 @@ class Hunt
             // set the owning side to null (unless already changed)
             if ($qrCode->getHunt() === $this) {
                 $qrCode->setHunt(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Question>
+     */
+    public function getQuestions(): Collection
+    {
+        return $this->questions;
+    }
+
+    public function addQuestion(Question $question): static
+    {
+        if (!$this->questions->contains($question)) {
+            $this->questions->add($question);
+            $question->setHunt($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestion(Question $question): static
+    {
+        if ($this->questions->removeElement($question)) {
+            // set the owning side to null (unless already changed)
+            if ($question->getHunt() === $this) {
+                $question->setHunt(null);
             }
         }
 
---
+++ src/Entity/Question.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'questions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Hunt $hunt = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $questionText = null;
+
+    #[ORM\Column]
+    private ?int $points = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $timeLimit = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getHunt(): ?Hunt
+    {
+        return $this->hunt;
+    }
+
+    public function setHunt(?Hunt $hunt): static
+    {
+        $this->hunt = $hunt;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getQuestionText(): ?string
+    {
+        return $this->questionText;
+    }
+
+    public function setQuestionText(string $questionText): static
+    {
+        $this->questionText = $questionText;
+
+        return $this;
+    }
+
+    public function getPoints(): ?int
+    {
+        return $this->points;
+    }
+
+    public function setPoints(int $points): static
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+
+    public function getTimeLimit(): ?int
+    {
+        return $this->timeLimit;
+    }
+
+    public function setTimeLimit(?int $timeLimit): static
+    {
+        $this->timeLimit = $timeLimit;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
